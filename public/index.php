a1c71b160268102e5d048abd9a5cc291c4570cf2
LapMarcos <marcoslaporte2015@gmail.com>
2023-06-24 23:41:15 -0300

    Base

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+// Error Handling
+error_reporting(-1);
+ini_set('display_errors', 1);
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Factory\AppFactory;
+use Slim\Routing\RouteCollectorProxy;
+use Slim\Routing\RouteContext;
+
+require __DIR__ . '\..\vendor\autoload.php';
